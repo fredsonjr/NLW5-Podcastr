831bc8b8973ff0499542498b7132c6266cc55602
Fredson Junior <fredson.cocco.jr@hotmail.com>
2021-04-24 17:04:51 -0300

    Commit 7

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -16,7 +16,9 @@ export function Player () {
         currentEpisodeIndex, 
         isPlaying, 
         togglePlay,
-        setPlayingState
+        setPlayingState,
+        playNext,
+        playPrevious
     } = useContext(PlayerContext)
 
     useEffect(() =>{
@@ -93,7 +95,7 @@ export function Player () {
                     <button type="button" disabled={!episode}>
                         <img src="/shuffle.svg" alt="Embaralhar"/>
                     </button>
-                    <button type="button" disabled={!episode}>
+                    <button type="button" onClick={playPrevious} disabled={!episode}>
                         <img src="/play-previous.svg" alt="Tocar anterior"/>
                     </button>
 
@@ -108,7 +110,7 @@ export function Player () {
                              : <img src="/play.svg" alt="Tocar"/> }
                     </button>
 
-                    <button type="button" disabled={!episode}>
+                    <button type="button" onClick={playNext} disabled={!episode}>
                         <img src="/play-next.svg" alt="Tocar próxima"/>
                     </button>
                     <button type="button" disabled={!episode}>
